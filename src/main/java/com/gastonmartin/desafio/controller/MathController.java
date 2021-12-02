48143ad84262d3b0c509812074b314198941ef0e
gaston.martin <gaston.martin@ank.app>
2021-12-02 19:42:07 -0300

    Agregado de Logging

--- src/main/java/com/gastonmartin/desafio/controller/MathController.java
+++ src/main/java/com/gastonmartin/desafio/controller/MathController.java
@@ -2,18 +2,23 @@ package com.gastonmartin.desafio.controller;
 
 import com.gastonmartin.desafio.model.AdditionRequest;
 import com.gastonmartin.desafio.model.AdditionResult;
+import lombok.extern.java.Log;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+import static java.lang.String.format;
+
+@Log
 @RestController("/v1/math")
 public class MathController {
 
     @PostMapping("/sum")
     @ResponseStatus(HttpStatus.OK)
     public AdditionResult sumTwoNumbers(@RequestBody final AdditionRequest numbers) {
+        log.info(format("Summing %f + %f", numbers.getLeft(), numbers.getRight()));
         return new AdditionResult(numbers.getLeft() + numbers.getRight());
     }
 }
